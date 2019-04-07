31750af2f7b8c1a96d11ee71c709b90d1caa6974
rogue-cricket <kristine@kristinerichard.com>
2019-04-07 11:40:32 -0300

    remainder app from page 45

---
+++ Remainder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simple remainder app from page 45.
+
+            int a = 22;
+            int b = 3;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine(a + "/" + b + " is " + quotient + " with remainder " + remainder);
+            Console.WriteLine("Checking work: " + quotient + "*" + b + " + " + remainder + " is " + (quotient * b + remainder));
+            Console.ReadKey();
+
+        }
+    }
+}
