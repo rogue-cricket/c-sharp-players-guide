292762ed95a72b20331a660ae099d572d27c64ac
rogue-cricket <kristine@kristinerichard.com>
2019-04-07 12:28:20 -0300

    switch calculator

---
+++ SwitchCalculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write your second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the operation between them (+, -, /, *, or ^):");
+            string op = Console.ReadLine();
+            string result;
+
+            switch (op)
+            {
+                case "+":
+                    result = "" + (num1 + num2);
+                    break;
+                case "-":
+                    result = "" + (num1 - num2);
+                    break;
+                case "/":
+                    result = "" + (num1 / num2);
+                    break;
+                case "*":
+                    result = "" + (num1 * num2);
+                    break;
+                case "^":
+                    result = "" + Math.Pow(num1, num2);
+                    break;
+                default:
+                    result = "not a real thing";
+                    break;
+            }
+
+            Console.WriteLine($"{num1}{op}{num2} is {result}.");
+            Console.ReadKey();
+        }
+    }
+}
