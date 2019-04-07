97c862e4c62436f71de36d734ea21183329544d5
rogue-cricket <kristine@kristinerichard.com>
2019-04-07 11:34:11 -0300

    triangle app from page 44

---
+++ TriangleArea/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Triangle app from the Try It Out! on page 44.
+            float baseLen = 5.0f;
+            float height = 6.0f;
+            float area = baseLen * height / 2;
+
+            Console.WriteLine("Area of triangle with base " + baseLen + " and height " + height + " is " + area);
+            Console.ReadKey();
+        }
+    }
+}
