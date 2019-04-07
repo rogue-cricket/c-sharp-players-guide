8886ef81b3b0d208f0ec66b6792ce8784a25dc9e
rogue-cricket <kristine@kristinerichard.com>
2019-04-07 12:15:17 -0300

    posi or negi

---
+++ EvenAndOdd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenAndOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The Try It Out! from page 65.  "Write a program that asks for user input and determines if it's even or odd."
+
+            Console.WriteLine("Enter your integer:");
+            int evenOrOdd = Convert.ToInt32(Console.ReadLine());
+
+            if (evenOrOdd % 2 == 0)
+                Console.WriteLine($"{evenOrOdd} is even!");
+            else
+                Console.WriteLine($"{evenOrOdd} is odd!");
+
+            Console.ReadKey();
+        }
+    }
+}
