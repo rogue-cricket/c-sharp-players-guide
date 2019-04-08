4d662168aba0604b97ba3d4ae5c96a2af1337d78
rogue-cricket <kristine@kristinerichard.com>
2019-04-08 17:29:17 -0300

    the reversal try it out for methods

---
+++ ReverseArray/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// The Try It Out! on page 82 - reversing an array using methods
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static int[] GenerateNumbers(int len)
+        {
+            int[] generatedArray = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                generatedArray[i] = i;
+            }
+            return generatedArray;
+        }
+
+        static int[] Reverse(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int tmp = arr[i];
+                arr[i] = arr[arr.Length - 1 - i];
+                arr[arr.Length - 1 - i] = tmp;
+
+            }
+            return arr;
+        }
+
+        static void PrintNumbers(int[] arr)
+        {
+            foreach (int num in arr)
+            {
+                Console.Write(num + ", ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = GenerateNumbers(10);
+            Reverse(numbers);
+            PrintNumbers(numbers);
+            Console.ReadKey();
+        }
+    }
+}
