98c0002938fa780b5e7a9afa6254f7a00ea042a9
rogue-cricket <kristine@kristinerichard.com>
2019-04-08 21:46:12 -0300

    example

---
+++ Book/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// This isn't a Try It Out! per se, but following allong with the Book class example starting on page 112.
+
+namespace Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Harry Potter", "J.K. Rowling");
+            book.SetTitle("Harry Potter and the Thingy");
+            Console.WriteLine(book.GetTitle());
+            Console.ReadKey();
+        }
+    }
+}
