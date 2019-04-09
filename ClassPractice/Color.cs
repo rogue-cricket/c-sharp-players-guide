e618ec42740b21bdff28df4a548585f28e7d8a3a
rogue-cricket <kristine@kristinerichard.com>
2019-04-09 16:39:15 -0300

    color kind of

--- ClassPractice/Color.cs
+++ ClassPractice/Color.cs
@@ -8,10 +8,78 @@ namespace ClassPractice
 {
     class Color
     {
-        int red;
-        int green;
-        int blue;
-        int alpha;
+        private int red;
+        private int green;
+        private int blue;
+        private int alpha;
+ 
+        public int Red
+        {
+            get
+            {
+                return red;
+            }
+            set
+            {
+                if (value > 255)
+                    red = 255;
+                else if (value < 0)
+                    red = 0;
+                else
+                    red = value;
+            }
+        }
+
+        public int Green
+        {
+            get
+            {
+                return green;
+            }
+            set
+            {
+                if (value > 255)
+                    green = 255;
+                else if (value < 0)
+                    green = 0;
+                else
+                    green = value;
+            }
+        }
+
+        public int Blue
+        {
+            get
+            {
+                return blue;
+            }
+            set
+            {
+                if (value > 255)
+                    blue = 255;
+                else if (value < 0)
+                    blue = 0;
+                else
+                    blue = value;
+            }
+        }
+
+        public int Alpha
+        {
+            get
+            {
+                return alpha;
+            }
+            set
+            {
+                if (value > 255)
+                    alpha = 255;
+                else if (value < 0)
+                    alpha = 0;
+                else
+                    alpha = value;
+            }
+        }
 
         public Color(int red, int green, int blue, int alpha)
         {
@@ -69,67 +137,6 @@ namespace ClassPractice
 
             this.alpha = 255;
 
-        }
-
-        public int GetRed()
-        {
-            return this.red;
-        }
-
-        public void SetRed(int red)
-        {
-            if (red > 255)
-                this.red = 255;
-            else if (red < 0)
-                this.red = 0;
-            else
-                this.red = red;
-        }
-
-        public int GetGreen()
-        {
-            return this.green;
-        }
-
-        public void SetGreen(int green)
-        {
-            if (green > 255)
-                this.green = 255;
-            else if (green < 0)
-                this.green = 0;
-            else
-                this.green = green;
-
-        }
-
-        public int GetBlue(int blue)
-        {
-            return this.blue;
-        }
-
-        public void SetBlue()
-        {
-            if (blue > 255)
-                this.blue = 255;
-            else if (blue < 0)
-                this.blue = 0;
-            else
-                this.blue = blue;
-        }
-
-        public int GetAlpha()
-        {
-            return this.alpha;
-        }
-
-        public void SetAlpha()
-        {
-            if (alpha > 255)
-                this.alpha = 255;
-            else if (alpha < 0)
-                this.alpha = 0;
-            else
-                this.alpha = alpha;
         }
 
         public int getGrayscale()
