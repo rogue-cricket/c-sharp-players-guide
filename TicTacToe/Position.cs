f1cad84b92e5d2333b8d2462251d0251858bbe28
rogue-cricket <kristine@kristinerichard.com>
2019-04-14 12:35:34 -0300

    donbe

---
+++ TicTacToe/Position.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Position
+    {
+        public int Row { get; }
+        public int Col { get; }
+        public char Content { get; set; }
+        public bool Filled { get; set; }
+
+        public Position(int row, int col, char content)
+        {
+            Row = row;
+            Col = col;
+            Content = content;
+            Filled = false;
+        }
+
+        /// <summary>
+        /// Attempts to place a marker in the position.
+        /// </summary>
+        /// <param name="marker">The marker to place.</param>
+        /// <returns>Returns true if the placement succeeded, false if it didn't</returns>
+        public bool PlaceMarker(char marker)
+        {
+            if (!Filled)
+            {
+                Content = marker;
+                Filled = true;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public char GetContent()
+        {
+            return Content;
+        }
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+            Game game = new Game();
+            game.Play();
+            Console.ReadKey();
+        }
+    }
+}
